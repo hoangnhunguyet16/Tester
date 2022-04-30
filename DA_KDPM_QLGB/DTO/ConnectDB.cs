a1219d1230f2030ff5ebef6f36788cfe03418806
d09tri <hodangtri392001@gmail.com>
2022-04-30 21:41:37 +0700

    Xây dựng giao diện FrmCauHinh và class ConnectDB để kết nối cơ sở dữ liệu

---
+++ DA_KDPM_QLGB/DTO/ConnectDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    [Serializable]
+    public class ConnectDB
+    {
+        public string ServerName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
+
+        public ConnectDB() { }
+
+        public ConnectDB(string serverName, string userName, string password, string database)
+        {
+            this.ServerName = serverName;
+            this.UserName = userName;
+            this.Password = password;
+            this.Database = database;
+        }
+
+        /// <summary>
+        /// Lưu file kết nối cơ sở dữ liệu vào trong Debug folder của project DTO
+        /// Trường hợp file đã tồn tại:
+        /// - Xóa file xong tiến hành tạo file lại 
+        /// - Để tránh xảy ra lỗi "The file is apparently being used by another process" trong quá trình xóa
+        /// ta sử dụng Garbage Collection (dòng 1, 2 trong if)
+        /// </summary>
+        public void SaveFileConnectDB()
+        {
+            if (File.Exists("connect-db.dba"))
+            {
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
+                File.Delete("connect-db.dba");
+            }
+
+            FileStream fs = File.Open("connect-db.dba", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Close();
+        }
+
+        public ConnectDB ReadFileConnectDB()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.Open("connect-db.dba", FileMode.Open, FileAccess.Read);
+            return (ConnectDB)bf.Deserialize(fs);
+        }
+    }
+}
--- DA_KDPM_QLGB/GUI/FrmCauHinh.Designer.cs
+++ DA_KDPM_QLGB/GUI/FrmCauHinh.Designer.cs
@@ -28,11 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grbThongTin = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbbServer = new System.Windows.Forms.ComboBox();
+            this.cbbDatabase = new System.Windows.Forms.ComboBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.btnDocFile = new System.Windows.Forms.Button();
+            this.btnLuuFile = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.grbThongTin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbThongTin
+            // 
+            this.grbThongTin.Controls.Add(this.btnThoat);
+            this.grbThongTin.Controls.Add(this.btnLuuFile);
+            this.grbThongTin.Controls.Add(this.btnDocFile);
+            this.grbThongTin.Controls.Add(this.btnKiemTra);
+            this.grbThongTin.Controls.Add(this.txtPassword);
+            this.grbThongTin.Controls.Add(this.txtUser);
+            this.grbThongTin.Controls.Add(this.cbbDatabase);
+            this.grbThongTin.Controls.Add(this.cbbServer);
+            this.grbThongTin.Controls.Add(this.label4);
+            this.grbThongTin.Controls.Add(this.label3);
+            this.grbThongTin.Controls.Add(this.label2);
+            this.grbThongTin.Controls.Add(this.label1);
+            this.grbThongTin.Location = new System.Drawing.Point(12, 12);
+            this.grbThongTin.Name = "grbThongTin";
+            this.grbThongTin.Size = new System.Drawing.Size(453, 234);
+            this.grbThongTin.TabIndex = 0;
+            this.grbThongTin.TabStop = false;
+            this.grbThongTin.Text = "Thông tin kết nối cơ sở dữ liệu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Database:";
+            // 
+            // cbbServer
+            // 
+            this.cbbServer.FormattingEnabled = true;
+            this.cbbServer.Location = new System.Drawing.Point(151, 55);
+            this.cbbServer.Name = "cbbServer";
+            this.cbbServer.Size = new System.Drawing.Size(208, 21);
+            this.cbbServer.TabIndex = 5;
+            // 
+            // cbbDatabase
+            // 
+            this.cbbDatabase.FormattingEnabled = true;
+            this.cbbDatabase.Location = new System.Drawing.Point(151, 134);
+            this.cbbDatabase.Name = "cbbDatabase";
+            this.cbbDatabase.Size = new System.Drawing.Size(208, 21);
+            this.cbbDatabase.TabIndex = 6;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(151, 82);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(208, 20);
+            this.txtUser.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(151, 108);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(208, 20);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Location = new System.Drawing.Point(70, 176);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(75, 23);
+            this.btnKiemTra.TabIndex = 9;
+            this.btnKiemTra.Text = "Kiểm tra";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            // 
+            // btnDocFile
+            // 
+            this.btnDocFile.Location = new System.Drawing.Point(151, 176);
+            this.btnDocFile.Name = "btnDocFile";
+            this.btnDocFile.Size = new System.Drawing.Size(75, 23);
+            this.btnDocFile.TabIndex = 10;
+            this.btnDocFile.Text = "Đọc file";
+            this.btnDocFile.UseVisualStyleBackColor = true;
+            // 
+            // btnLuuFile
+            // 
+            this.btnLuuFile.Location = new System.Drawing.Point(232, 176);
+            this.btnLuuFile.Name = "btnLuuFile";
+            this.btnLuuFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLuuFile.TabIndex = 11;
+            this.btnLuuFile.Text = "Lưu file";
+            this.btnLuuFile.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(313, 176);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 12;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // FrmCauHinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmCauHinh";
+            this.ClientSize = new System.Drawing.Size(477, 258);
+            this.Controls.Add(this.grbThongTin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmCauHinh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cấu hình kết nối cơ sở dữ liệu";
+            this.grbThongTin.ResumeLayout(false);
+            this.grbThongTin.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbThongTin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.ComboBox cbbDatabase;
+        private System.Windows.Forms.ComboBox cbbServer;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnLuuFile;
+        private System.Windows.Forms.Button btnDocFile;
+        private System.Windows.Forms.Button btnKiemTra;
     }
 }
